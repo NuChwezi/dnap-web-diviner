26a031073d96ed4db91fce653df7a4c4f8c51ce6
Nemesis Fixx <joewillrich@gmail.com>
2016-10-11 09:27:13 +0000

    there's a better way to load and use the meliorator

--- js/index.js
+++ js/index.js
@@ -45,7 +45,8 @@ function getUrlVars()
     return vars;
 }
 
-$(document).ready(function() {
+// do everything only after meliorator fully loads
+$(document).on('Meliorator-Ready',function() {
 
 
     $('#btn-fetch').click(function() {
@@ -69,9 +70,9 @@ $(document).ready(function() {
                 initPanels(JSON.parse(response));
             }).fail(function(){
 
-            $('.chart.widget').empty().append($('<p/>', {
-                'style': 'color: #ff18c1;font-weight:bold;font-size:large;'
-            }).html('SORRY! We failed to load data and the analytics panel via the given parameters. Cross-check them well, and try again.'))
+                $('.chart.widget').empty().append($('<p/>', {
+                    'style': 'color: #ff18c1;font-weight:bold;font-size:large;'
+                }).html('SORRY! We failed to load data and the analytics panel via the given parameters. Cross-check them well, and try again.'))
 
 
             });
@@ -98,69 +99,70 @@ $(document).ready(function() {
             }
 
             $('#data-url').val(analyticsParams.url)
-            $('#access-token').val(analyticsParams.token || analyticsParams.access_token || analyticsParams.t || "")
-            // in this case, the data is obtained via an api, and then we pass the result to 
-            // Meliorator
-            $.ajax(settings).done(function(response) {
-                initPanels(JSON.parse(response));
-            });
-
-        }
-
-        }else { 
-            var query = getUrlVars();
-            if(query.url != undefined){
-
-                // let's init the data analysis from the data-source in the url
-                var settings = {
-                    "async": true,
-                    "crossDomain": true,
-                    "url": query.url,
-                    "method": query.method || "POST",
-                    "data": query,
-                }
-
-                $('#data-url').val(query.url)
-                    $('#access-token').val(query.token || query.access_token || query.t || "")
-                    // in this case, the data is obtained via an api, and then we pass the result to 
-                    // Meliorator
-                    $.ajax(settings).done(function(response) {
-                        initPanels(JSON.parse(response));
-                    });
-
-
-            }else {
-
-                // let's make lorem objects with which to test our lib...
-                // we'll use the awesome schematic-ipsum API for this..
-                var settings = {
-                    "async": true,
-                    "crossDomain": true,
-                    "url": "https://schematic-ipsum.herokuapp.com/?n=50",
-                    "method": "POST",
-                    "headers": {
-                        "content-type": "application/json"
-                    },
-                    "data": "{\n  \"type\": \"object\",\n  \"properties\": {\n    \"Name\": { \"type\": \"string\", \"ipsum\": \"name\" },\n    \"Group\": {\n  \"type\": \"string\",\n  \"enum\": [\"Plankton\", \"Islander\", \"Mystic\", \"Veterans\"]\n},\n    \"City\": {\n  \"type\": \"string\",\n  \"enum\": [\"Entebbe\", \"Kampala\", \"Jinja\", \"Masindi\", \"Gulu\", \"Masaka\", \"Mbarara\"]\n},\n    \"Age\": { \"type\": \"integer\" },\n\t  \"Score\": { \"type\": \"number\" },\n\t  \"Rank\": { \"type\": \"integer\" }\n   \n  }\n}"
-                }
-
+                $('#access-token').val(analyticsParams.token || analyticsParams.access_token || analyticsParams.t || "")
                 // in this case, the data is obtained via an api, and then we pass the result to 
                 // Meliorator
                 $.ajax(settings).done(function(response) {
                     initPanels(JSON.parse(response));
-                    setTimeout(function(){
-                        welcome() }, 2000);
                 });
 
+        }
+
+    }else { 
+        $('.app-title').text("");
+        var query = getUrlVars();
+        if(query.url != undefined){
+
+            // let's init the data analysis from the data-source in the url
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": query.url,
+                "method": query.method || "POST",
+                "data": query,
             }
 
+            $('#data-url').val(query.url)
+                $('#access-token').val(query.token || query.access_token || query.t || "")
+                // in this case, the data is obtained via an api, and then we pass the result to 
+                // Meliorator
+                $.ajax(settings).done(function(response) {
+                    initPanels(JSON.parse(response));
+                });
+
+
+        }else {
+
+            // let's make lorem objects with which to test our lib...
+            // we'll use the awesome schematic-ipsum API for this..
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://schematic-ipsum.herokuapp.com/?n=50",
+                "method": "POST",
+                "headers": {
+                    "content-type": "application/json"
+                },
+                "data": "{\n  \"type\": \"object\",\n  \"properties\": {\n    \"Name\": { \"type\": \"string\", \"ipsum\": \"name\" },\n    \"Group\": {\n  \"type\": \"string\",\n  \"enum\": [\"Plankton\", \"Islander\", \"Mystic\", \"Veterans\"]\n},\n    \"City\": {\n  \"type\": \"string\",\n  \"enum\": [\"Entebbe\", \"Kampala\", \"Jinja\", \"Masindi\", \"Gulu\", \"Masaka\", \"Mbarara\"]\n},\n    \"Age\": { \"type\": \"integer\" },\n\t  \"Score\": { \"type\": \"number\" },\n\t  \"Rank\": { \"type\": \"integer\" }\n   \n  }\n}"
+            }
+
+            // in this case, the data is obtained via an api, and then we pass the result to 
+            // Meliorator
+            $.ajax(settings).done(function(response) {
+                initPanels(JSON.parse(response));
+                setTimeout(function(){
+                    welcome() }, 2000);
+            });
+
         }
+
+    }
 }    );
 
 
-        function welcome(){
+function welcome(){
 
-            $('.chart.widget').empty().append($('<p/>', {
-                'style': 'color: rgba(41, 0, 0, 0.74);font-weight:bold;font-size:large;'
-            }).html('WELCOME to the Data Diviner, the most beautiful, free, advanced data hero in the world. <br/><hr/> Initially, we load for you some basic, random sample data from a lorem data service. This is just to help you play around with this analytical tool, then when you know how it works, go find your own data (build a free app at <a href="https://studio.nuchwezi.com" target="_blank">The Studio</a>, it gives you a free Data Diviner for the data posted via your app.) <br/> <hr/>Otherwise, please provide proper parameters for fetching the data... A URL and where required, an access token. <br/>Note: the diviner can analyze any data of the form: <br/><br/><i>[{field1:val1, field2:val2,...}, {field1:valn, field2:valn2,...},... ]<i>'))
-        }
+    $('.chart.widget').empty().append($('<p/>', {
+        'style': 'color: rgba(41, 0, 0, 0.74);font-weight:bold;font-size:large;'
+    }).html('WELCOME to the Data Diviner, the most beautiful, free, advanced data hero in the world. <br/><hr/> Initially, we load for you some basic, random sample data from a lorem data service. This is just to help you play around with this analytical tool, then when you know how it works, go find your own data (build a free app at <a href="https://studio.nuchwezi.com" target="_blank">The Studio</a>, it gives you a free Data Diviner for the data posted via your app.) <br/> <hr/>Otherwise, please provide proper parameters for fetching the data... A URL and where required, an access token. <br/>Note: the diviner can analyze any data of the form: <br/><br/><i>[{field1:val1, field2:val2,...}, {field1:valn, field2:valn2,...},... ]<i>'))
+}
